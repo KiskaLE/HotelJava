e594afae0e5a55163490d7f866b56196d5bcf2cf
KiskaLE <vojtakylar@seznam.cz>
2022-02-08 13:49:20 +0100

    changes

--- src/StartApp/core.java
+++ src/StartApp/core.java
@@ -18,15 +18,13 @@ public class core {
         Hotel hotel = new Hotel("U Pardubic", 10);
         
         
-        System.out.println(hotel.setRooms(new Room[]{
-            new Room(1 ,2), new Room(2, 2), new Room(3, 2), new Room(4, 2), new Room(5, 2)
-        }));
+        System.out.println(hotel.setRooms(new Room[]{new Room(0,2), new Room(1, 2), new Room(2, 2), new Room(3, 2), new Room(4, 2), new Room(5, 2), new Room(6, 2), new Room(7, 2), new Room(8, 2), new Room(9, 2)}));
         
         System.out.println(hotel.addHosts(0, new Host[]{new Host("Pavel", "Nevim"), new Host("Tomáš", "Nevim"),new Host("Tomáš", "Nevim") }));
-        System.out.println(hotel.addHosts(0, new Host[]{new Host("Pavel", "Nevim"), new Host("Tomáš", "Nevim")}));
-        System.out.println(hotel.addHosts(0, new Host[]{new Host("Pavel", "Nevim"), new Host("Tomáš", "Nevim")}));
-        System.out.println(hotel.addHosts(0, new Host[]{new Host("Pavel", "Nevim"), new Host("Tomáš", "Nevim")}));
-        System.out.println(hotel.addHosts(0, new Host[]{new Host("Pavel", "Nevim"), new Host("Tomáš", "Nevim")}));
+        System.out.println(hotel.addHosts(1, new Host[]{new Host("Pavel", "Nevim"), new Host("Tomáš", "Nevim")}));
+        System.out.println(hotel.addHosts(2, new Host[]{new Host("Pavel", "Nevim"), new Host("Tomáš", "Nevim")}));
+        System.out.println(hotel.addHosts(3, new Host[]{new Host("Pavel", "Nevim"), new Host("Tomáš", "Nevim")}));
+        System.out.println(hotel.addHosts(4, new Host[]{new Host("Pavel", "Nevim")}));
         
         
         
